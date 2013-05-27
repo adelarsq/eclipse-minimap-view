9d888b9a8e28cc099ea74f8f4b557e09d77c58fd
Adelar da Silva Queiróz <adelarsq@gmail.com>
2013-05-27 10:07:32 -0300

    Corrections.

--- com.pauzies.minimap/src/com/pauzies/minimap/views/MiniMapView.java
+++ com.pauzies.minimap/src/com/pauzies/minimap/views/MiniMapView.java
@@ -2,6 +2,7 @@ package com.pauzies.minimap.views;
 
 import java.lang.reflect.Method;
 
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
@@ -10,6 +11,8 @@ import org.eclipse.swt.custom.BidiSegmentListener;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.ControlListener;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
@@ -20,6 +23,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.ISelectionListener;
@@ -36,16 +40,18 @@ public class MiniMapView extends ViewPart {
 	private Color selectionBackgroundColor;
 	private Color selectionForegroundColor;
 
-	private Font font = new Font(Display.getCurrent(), "Arial", 4, SWT.NONE);
 	private AbstractTextEditor selectedPart;
 	private MyTextEditor editor;
 	private boolean mouseIsDown;
 	private StyledText minimap;
+	
+	private float scale = 0.5f;
 
 	class MyTextEditor {
 		private final AbstractTextEditor editor;
 
 		public MyTextEditor(AbstractTextEditor editor) {
+		  System.out.println("MyTextEditor");
 			this.editor = editor;
 		}
 
@@ -62,28 +68,26 @@ public class MiniMapView extends ViewPart {
 	}
 	
 	private static void fillMiniMap(StyledText minimap, MyTextEditor editor) {
+	  System.out.println("fillMiniMap");
 		StyledText text = editor.getSourceViewer().getTextWidget();
 		minimap.setContent(text.getContent());
 		minimap.setStyleRanges(editor.getSourceViewer().getTextWidget().getStyleRanges());
 	}
 
 	private void highlightVisibleRegion(StyledText minimap, MyTextEditor editor) {
-		int start = editor.getSourceViewer().getTopIndexStartOffset();
-		int end = editor.getSourceViewer().getBottomIndexEndOffset();
-		int startLine = minimap.getLineAtOffset(start);
-		int endLine = minimap.getLineAtOffset(end);
-		// Reset
+		int startLine = editor.getSourceViewer().getTopIndex();
+    int endLine = editor.getSourceViewer().getBottomIndex();
+    //reset background color for whole document
 		minimap.setLineBackground(0, minimap.getLineCount() - 1, backgroundColor);
+		// set visible area colour
 		minimap.setLineBackground(startLine, endLine - startLine + 1, selectionBackgroundColor);
-		// minimap.setSelection(minimap.getOffsetAtLine(startLine),
-		// minimap.getOffsetAtLine(endLine - startLine +1));
 	}
 
 	private void changeEditor(MouseEvent e) {
 		StyledText text = (StyledText) e.getSource();
 		int line = text.getLineAtOffset(text.getCaretOffset());
 		// editor.getSourceViewer().getTextWidget().setCaretOffset(text.getCaretOffset());
-		editor.getSourceViewer().getTextWidget().setTopIndex(line);
+//		editor.getSourceViewer().getTextWidget().setTopIndex(line);
 	}
 
 	private final MouseListener minimapMouseListener = new MouseAdapter() {
@@ -146,6 +150,7 @@ public class MiniMapView extends ViewPart {
 
 		@Override
 		public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+		  System.out.println("selectionChanged -> " + part.getTitle());
 			if (part == selectedPart) {
 				return;
 			}
@@ -160,6 +165,8 @@ public class MiniMapView extends ViewPart {
 			selectedPart = (AbstractTextEditor) part;
 			editor = new MyTextEditor((AbstractTextEditor) part);
 
+			setMinimapFont();
+			
 			fillMiniMap(minimap, editor);
 			highlightVisibleRegion(minimap, editor);
 
@@ -168,7 +175,7 @@ public class MiniMapView extends ViewPart {
 
 			foregroundColor = editor.getSourceViewer().getTextWidget().getForeground();
 			backgroundColor = editor.getSourceViewer().getTextWidget().getBackground();
-			selectionBackgroundColor = editor.getSourceViewer().getTextWidget().getSelectionBackground();
+			selectionBackgroundColor = editor.getSourceViewer().getTextWidget().getSelectionBackground(); //TODO : should get another color than selected color
 			selectionForegroundColor = editor.getSourceViewer().getTextWidget().getSelectionForeground();
 
 			minimap.setBackground(backgroundColor);
@@ -188,11 +195,11 @@ public class MiniMapView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
+	  System.out.println("createPartControl");
 
 		minimap = new StyledText(parent, SWT.NONE);
 		minimap.setEditable(false);
 		// minimap.setEnabled(true);
-		minimap.setFont(font);
 		// minimap.setCursor(null);
 		minimap.setCaret(null);
 
@@ -217,5 +224,27 @@ public class MiniMapView extends ViewPart {
 		editor.getSourceViewer().getTextWidget().removeBidiSegmentListener(editorSegmentListener);
 		getSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(editorSelectionListener);
 	}
+	
+	private void setMinimapFont() {
+		System.out.println("setMinimapFontAndScale");
+		// create font for minimap that matches original and set to scale
+		Font origFont = JFaceResources.getFont(JFaceResources.TEXT_FONT);
+		FontData[] fontData = origFont.getFontData();
+		int origFontSize = 0;
+		for (int i = 0; i < fontData.length; i++) {
+			System.out.println("fontdata " + i);
+			origFontSize = fontData[i].getHeight();
+			fontData[i].setHeight((int) (origFontSize * scale));
+		}
 
+		final Font miniFont = new Font(Display.getCurrent(), fontData);
+		minimap.setFont(miniFont);
+
+		// dispose font
+		minimap.addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+				miniFont.dispose();
+			}
+		});
+	}
 }
